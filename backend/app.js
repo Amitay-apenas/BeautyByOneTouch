cdb7bf0b44d293de881ccac3ecf2f0d578b4ff75
Amitay-apenas <bamitay092@gmail.com>
2025-08-09 12:32:56 -0300

    a

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
-const path = require('path');
 
 dotenv.config({ path: './.env' });
 
@@ -10,15 +9,18 @@ connectDB();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 app.use(express.json());
 app.use(cors());
+const path = require('path');
 
+// Serve os arquivos estáticos da pasta 'dist' dentro de 'frontend'
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+
+// Redireciona todas as outras requisições para o index.html
+// Isso é essencial para que o roteamento do seu frontend funcione
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
-
-app.use('/api/profissionais', require('./src/routes/profissionais'));
 
 const PORT = process.env.PORT || 5000;
 

3ad6a1e5449ce62e1981b35b38070fddd6feda12
Amitay-apenas <bamitay092@gmail.com>
2025-08-09 11:53:51 -0300

    pre-deploy

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
+const path = require('path');
 
 dotenv.config({ path: './.env' });
 
@@ -9,8 +10,13 @@ connectDB();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(express.json());
 app.use(cors());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.use('/api/profissionais', require('./src/routes/profissionais'));
 

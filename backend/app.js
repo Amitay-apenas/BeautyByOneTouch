31cc4f58e4b5ba400f4052cbd512d6cce76bc2c8
Amitay-apenas <bamitay092@gmail.com>
2025-08-09 11:59:27 -0300

    pre-deploy

--- backend/app.js
+++ backend/app.js
@@ -10,12 +10,12 @@ connectDB();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 app.use(express.json());
 app.use(cors());
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
 app.use('/api/profissionais', require('./src/routes/profissionais'));
@@ -23,5 +23,5 @@ app.use('/api/profissionais', require('./src/routes/profissionais'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });

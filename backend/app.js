f0b3bf9118a087779f6a9ee7354bd4f55b32dd4a
Amitay-apenas <bamitay092@gmail.com>
2025-08-09 18:42:00 -0300

    a

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,9 @@ app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
 // 3. Defina a rota "catch-all" por último
 // Ela serve o index.html para todas as outras requisições
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+// });
 
 const PORT = process.env.PORT || 10000;
 

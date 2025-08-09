0c597558d451c4b13d59b606ca0a6858b10e2b4f
Amitay-apenas <bamitay092@gmail.com>
2025-08-09 12:56:44 -0300

    a

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/api/profissionais', require('./src/routes/profissionais'));
 const path = require('path');
 
 // Serve os arquivos estáticos da pasta 'dist' dentro de 'frontend'
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 // Redireciona todas as outras requisições para o index.html
 // Isso é essencial para que o roteamento do seu frontend funcione
 app.get('*', (req, res) => {
+  
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 

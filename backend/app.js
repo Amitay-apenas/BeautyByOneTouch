3b0f68005a34ebec4becd0aa7fe85c5b9cf31a4f
Amitay-apenas <bamitay092@gmail.com>
2025-08-09 18:29:26 -0300

    deploy

--- backend/app.js
+++ backend/app.js
@@ -10,20 +10,21 @@ connectDB();
 
 const app = express();
 
-// Comente todas essas linhas
 app.use(express.json());
 app.use(cors());
-
-const profissionaisRouter = require('./src/routes/profissionais.js');
-app.use('/api/profissionais', profissionaisRouter);
 
 // Serve os arquivos estáticos do frontend (CSS, JS, etc.)
 app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
-// Para qualquer outra rota que não seja da API, redirecione para o index.html
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+// 1. Defina as rotas da sua API aqui
+const profissionaisRouter = require('./src/routes/profissionais.js');
+app.use('/api/profissionais', profissionaisRouter);
+
+// 2. Para qualquer outra rota que não seja da API, redirecione para o index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
+
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
